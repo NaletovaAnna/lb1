1d96d6cf8508059e79a4bdf62c0ed2aa4290729d
NaletovaAnna <124901163+NaletovaAnna@users.noreply.github.com>
2023-03-15 16:35:19 +0300

    лаба 3

---
+++ 3.1.py
@@ -0,0 +1,10 @@
+s = ""
+a = []
+
+while True:
+    s = input()
+    if not s:
+        break
+    a.append(s)
+
+print(" ".join(a))
---
+++ 3.2.py
@@ -0,0 +1,7 @@
+s = ""
+a = []
+
+while  (new_s := str(input())) != "stop":
+    a.append(new_s)
+
+print(" ".join(a))
